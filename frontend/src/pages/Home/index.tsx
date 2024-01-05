0d928d8234a99ee07d21efdcab6132581a825689
Andoni Laurentiu <iliuta.andoni01@e-uvt.ro>
2024-01-05 17:57:46 +0200

    get cafenele din baza de date CR flavia

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,23 +1,40 @@
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 import MarkerCoffee from "../../assets/icons/Marker_Coffee.svg";
-import { useState } from "react";
 import Navbar from "./Navbar";
+import axios from "axios";
+
+interface coffeeShop {
+  Name: string;
+  Geolocation: [number, number];
+  ManagerId: number;
+  Coffees: [string];
+  Address: string;
+  Availabilities: [string];
+  ServiceType: string;
+  Description: string;
+  Photos: [string];
+}
 
 const Home = () => {
-  const [markers, setMarkers] = useState<JSX.Element[]>([
-    <MarkerF
-      position={{ lat: 44.4346955, lng: 26.1001067 }}
-      icon={MarkerCoffee}
-    />,
-    <MarkerF
-      position={{ lat: 44.4350205, lng: 26.098955 }}
-      icon={MarkerCoffee}
-    />,
-    <MarkerF
-      position={{ lat: 44.4350205, lng: 26.09915 }}
-      icon={MarkerCoffee}
-    />,
-  ]);
+  const [markers, setMarkers] = useState<JSX.Element[]>([]);
+  const addMarker = (lat: number, long: number) => {
+    setMarkers((markers) => [
+      ...markers,
+      <MarkerF
+        draggable
+        position={{ lat: lat, lng: long }}
+        icon={MarkerCoffee}
+      />,
+    ]);
+  };
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_URL}coffeeShops`).then((response) => {
+      response.data.map((item: coffeeShop) => {
+        addMarker(item.Geolocation[0], item.Geolocation[1]);
+      });
+    });
+  }, []);
 
   const [coord, setCoord] = useState<{ lat: number; lng: number }>({
     lat: 44.4355355,
@@ -46,22 +63,6 @@ const Home = () => {
       ) : (
         <div className="absolute">
           <Navbar />
-
-          <div
-            className="absolute z-[1] top-[160px] left-[5px] w-[100px] h-[50px] bg-brand-main text-brand-light text-center rounded-md"
-            onClick={() =>
-              setMarkers([
-                ...markers,
-                <MarkerF
-                  draggable
-                  position={{ lat: 44.4355355, lng: 26.0995867 }}
-                  icon={MarkerCoffee}
-                />,
-              ])
-            }
-          >
-            Add another Coffee Shop
-          </div>
           <GoogleMap
             mapContainerClassName="h-screen w-screen absolute"
             center={coord}
