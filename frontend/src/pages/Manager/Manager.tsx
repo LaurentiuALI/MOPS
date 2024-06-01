e4865886ee65883c832b8cf0fcf4a9345aefb4f2
flavia121duta <flavia.duta@s.unibuc.ro>
2024-06-01 17:05:41 +0300

    change icong when editing schedule

--- frontend/src/pages/Manager/Manager.tsx
+++ frontend/src/pages/Manager/Manager.tsx
@@ -21,6 +21,7 @@ import latte from "../../assets/images/coffees/latte.png";
 import mocha from "../../assets/images/coffees/mocha.png";
 import { FaPlusCircle } from "react-icons/fa";
 import SVGPen from "../../components/Icons/SVGPen";
+import Row from "../../components/Row/Row";
 
 const coffeesAvailable = {
   0: americano,
@@ -113,11 +114,11 @@ const Manager = () => {
               onClick={() => setMenuIsOpen((prev) => !prev)}
             />
           </header>
-          <div className="flex center justify-between w-full">
+          <div className="flex center justify-between w-full pr-[16px]">
           <p contentEditable id="cofee-shop-address" className="text-brand-secondary text-sm font-normal p-4">
             {coffeeShop?.Address || "Address"}
           </p>
-          <SVGPen title="edit" className="mt-4" onClick={()=>{
+          <SVGPen title="edit" className="mt-4" color="#FBF6F2" onClick={()=>{
             const newAddress = document.getElementById("cofee-shop-address")?.innerText
             if(newAddress != coffeeShop?.Address && (typeof newAddress == 'string')){
               console.log(newAddress);
@@ -225,49 +226,50 @@ const Manager = () => {
                 <h2 className="text-[20px] font-bold leading-[130%] text-brand-black">
                   Serving coffee on
                 </h2>
-                {coffeeShop?.Availabilities.map((day, index) => (
-                  <div key={`schedule-${index}`}>
-                    <div className="flex justify-between w-full">
-                      <p className="text-[14px] font-normal leading-[150%] text-brand-main w-full">
-                        {day.workDay}
-                      </p>
-                      <p className="w-full"><span contentEditable id={`schedule-${index}-opening`}>{day.openingHour}</span> - <span contentEditable id={`schedule-${index}-closing`}>{day.closingHour}</span></p>
-                      <SVGPen className="w-[32px]" title="edit" onClick={
-                        ()=>{
-                          const newOpeningHour = document.getElementById(`schedule-${index}-opening`)?.innerHTML
-                          const newClosingHour = document.getElementById(`schedule-${index}-closing`)?.innerHTML
-                          if(
-                            day.openingHour != newOpeningHour && (typeof newOpeningHour == 'string')
-                            ||
-                            day.closingHour != newClosingHour && (typeof newClosingHour == 'string')
-                          ){
+                {coffeeShop?.Availabilities.map((scheduleData, index) => (
+                  // <div key={`schedule-${index}`}>
+                  //   <div className="flex justify-between w-full">
+                  //     <p className="text-[14px] font-normal leading-[150%] text-brand-main w-full">
+                  //       {day.workDay}
+                  //     </p>
+                  //     <p className="w-full"><span contentEditable id={`schedule-${index}-opening`}>{day.openingHour}</span> - <span contentEditable id={`schedule-${index}-closing`}>{day.closingHour}</span></p>
+                  //     <SVGPen className="w-[32px]" title="edit" onClick={
+                  //       ()=>{
+                  //         const newOpeningHour = document.getElementById(`schedule-${index}-opening`)?.innerHTML
+                  //         const newClosingHour = document.getElementById(`schedule-${index}-closing`)?.innerHTML
+                  //         if(
+                  //           day.openingHour != newOpeningHour && (typeof newOpeningHour == 'string')
+                  //           ||
+                  //           day.closingHour != newClosingHour && (typeof newClosingHour == 'string')
+                  //         ){
                             
-                              // update
-                              const newSchedule =[...coffeeShop.Availabilities]
-                              newSchedule[index] = {
-                                workDay: day.workDay,
-                                openingHour: newOpeningHour!,
-                                closingHour: newClosingHour!,
-                              }
-                              axios.put(`${import.meta.env.VITE_URL}coffeeshop/manager/${coffeeShop.Name}/schedule`,{
-                                newSchedule: newSchedule
-                              }).then(data=>{
-                                console.log(data);
+                  //             // update
+                  //             const newSchedule =[...coffeeShop.Availabilities]
+                  //             newSchedule[index] = {
+                  //               workDay: day.workDay,
+                  //               openingHour: newOpeningHour!,
+                  //               closingHour: newClosingHour!,
+                  //             }
+                  //             axios.put(`${import.meta.env.VITE_URL}coffeeshop/manager/${coffeeShop.Name}/schedule`,{
+                  //               newSchedule: newSchedule
+                  //             }).then(data=>{
+                  //               console.log(data);
                                 
-                              }).catch(error=>{
-                                console.log(error);
+                  //             }).catch(error=>{
+                  //               console.log(error);
                                 
-                              })
-                            }else{
-                              const element = document.getElementById(`schedule-${index}-opening`)
-                              element?.focus()
-                            }
+                  //             })
+                  //           }else{
+                  //             const element = document.getElementById(`schedule-${index}-opening`)
+                  //             element?.focus()
+                  //           }
                           
-                        }
-                      } />
-                    </div>
-                    <div className="h-[1px] w-full border border-brand-borderDark"></div>
-                  </div>
+                  //       }
+                  //     } />
+                  //   </div>
+                  //   <div className="h-[1px] w-full border border-brand-borderDark"></div>
+                  // </div>
+                  <Row index={index} coffeeShop={coffeeShop} scheduleData={scheduleData} />
                 ))}
               </>
             )}
